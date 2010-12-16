51447cb1583f12d3b01ed9777a7d460b7a628055
Kent Fredric <kentfredric@gmail.com>
2010-12-16 21:21:12 +1300

    now have working static file serving!

--- lib/Dispatcher/Action/StaticFile.php
+++ lib/Dispatcher/Action/StaticFile.php
@@ -9,23 +9,72 @@ class Dispatcher_Action_StaticFile implements Dispatcher_Action {
     $this->opts = $opts;
   }
 
-  public function _deliver_file( $rawname ){ 
+  public function _deliver_file( $rawname, $type ){ 
 
+    header("Content-Type: $type");
+    readfile( $rawname );
 
   }
 
-  public function perform( ){ 
+  public function _filename(){
     if( array_key_exists( 'source_param' , $this->opts ) ){
       $keyname = $this->opts['source_param'];
       if( array_key_exists($keyname, $this->setparams ) ){ 
-        return $this->_deliver_file( $this->setparams[$keyname]);
+        return $this->setparams[$keyname];
       }
-      # Crap.
-    } elseif ( array_key_exists('filename' , $this->opts ) ){ 
-      return $this->_deliver_file( $this->opts['filename'] );
-    } else {
-      # CRAP.
     }
+    if( array_key_exists('filename' , $this->opts ) ){
+      return $this->opts['filename'];
+    }
+  }
+  public function _typemap(){
+    if( ! array_key_exists( 'typemap' , $this->opts ) ){ 
+      $this->opts['typemap'] = 'default';
+    }
+    if( $this->opts['typemap'] !== 'default' ){ 
+      return $this->opts['typemap'];
+    }
+    $defaultmap = array( 
+      "::DEFAULT::" => 'application/octet-stream',
+      '.php'        => 'text/plain',
+      '.txt'        => 'text/plain',
+      '.css'        => 'text/css',
+      '.html'       => 'text/html', 
+      '.png'        => 'image/png', 
+      '.jpg'        => 'image/jpeg', 
+      '.gif'        => 'image/gif',
+      '.js'         => 'application/javascript',
+    );
+    return $defaultmap;
+
+  }
+  public function _type() {
+    if( ! array_key_exists( 'type' , $this->opts ) ){ 
+      $this->opts['type'] = 'auto';
+    }
+    if( $this->opts['type'] === 'auto' ){ 
+      $typemap = $this->_typemap();
+      #print_r($typemap); die;
+      $pathinfo = pathinfo( $this->_filename() ); 
+      $extension = '.' . $pathinfo['extension'];
+      if( array_key_exists( $extension , $typemap ) ){ 
+        return $typemap[$extension];
+      }
+      if( array_key_exists( '::DEFAULT::', $typemap )){ 
+        return $typemap['::DEFAULT::'];
+      }
+      return 'application/octet-stream';
+    }
+    return $this->opts['type'];
+  }
+
+  public function perform( ){ 
+    $filename = $this->_filename();
+    if( !$filename ){ 
+      #?die?
+    }
+    $type = $this->_type();
+    return $this->_deliver_file( $filename, $type );
   }
 
   public function set_param( array $params ){
